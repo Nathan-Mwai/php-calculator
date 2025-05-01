6ceffe43ed9ca8b56bcc77e11d059b144247e9d3
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-05-01 12:14:01 +0300

    implemented calculator

--- index.php
+++ index.php
@@ -1,14 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
+
 <body>
     <section>
-        <form action="" method="get"></form>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <input type="number" name="num01" required id="num1" placeholder="1">
+            <select name="operator">
+                <option value="add">+</option>
+                <option value="subtract">-</option>
+                <option value="multiply">*</option>
+                <option value="divide">/</option>
+            </select>
+            <input type="number" name="num02" required placeholder="2">
+            <button type="submit">Calculate</button>
+        </form>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            //Grabbing data from inputs 
+            // filter_input is for increasing security
+            $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
+            $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
+            $operator = htmlspecialchars($_POST["operator"]);
+
+            // error handling
+            $errors = false;
+            //empty checks if variable is empty
+            if (empty($num01) || empty($num02) || empty($operator)) {
+                echo "<h3>Fill in all fields!</h3>";
+                $errors = true;
+            }
+
+            if (!is_numeric($num01) || !is_numeric($num02)) {
+                echo "<h3>Only write numbers!</h3>";
+                $errors = true;
+            }
+
+            // calculate number if no errors
+            if (!$errors) {
+                $value = 0;
+                switch ($operator) {
+                    case "add":
+                        $value = $num01 + $num02;
+                        break;
+                    case "subtract":
+                        $value = $num01 - $num02;
+                        break;
+                    case "multiply":
+                        $value = $num01 * $num02;
+                        break;
+                    case "divide":
+                        $value = $num01 / $num02;
+                        break;
+                    default:
+                        echo "<h3>Something went HORRIBLY wrong!</h3>";
+                }
+                echo "<p class='calc-result'>Result = {$value} </p>";
+            }
+        }
+        ?>
     </section>
 </body>
+
 </html>
